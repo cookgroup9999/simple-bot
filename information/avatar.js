550db52a5ffc0103d6d890dcec41570ad5bb1a92
DevFurry-1064 <82998889+DevFurry-1064@users.noreply.github.com>
2021-07-07 15:20:08 +0700

    avatar.js

--- information/avatar.js
+++ information/avatar.js
@@ -1,16 +1,19 @@
+const Discord = require('discord.js')
+const { Color } = require("../../config.js")
+
 module.exports = {
   name: "avatar",
   aliases: ["av", "pfp", "icon"],
   description: "Avatar",
   usage: "Get user avatar",
-  run: async(client, message, args, Color, Discord) => {
+  run: async(client, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
     
     let embed = new Discord.MessageEmbed()
-    .setAuthor(member.user.tag, member.user.displayAvatarURL({dynamic: true})
-    .setImage(member.user.displayAvatarURL({dynamic: true})
-    .setDescription(`[Click here for avatar page](${member.user.displayAvatarURL({dynamic: true})}`)
-     .setColor(Color)
+    .setAuthor(member.user.tag, member.user.displayAvatarURL({dynamic: true}))
+    .setImage(member.user.displayAvatarURL({size: 1024}))
+    .setDescription(`[Click here for an animated version of the Avatar!](${member.user.displayAvatarURL({dynamic: true})})`)
+    .setColor(client.color)
               
       message.channel.send(embed)
   }
