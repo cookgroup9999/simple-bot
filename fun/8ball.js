48a298ec4d455ad585e8bbb667096f79becdc577
DevFurry-1064 <82998889+DevFurry-1064@users.noreply.github.com>
2021-07-08 15:59:55 +0700

    8ball.js

---
+++ fun/8ball.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+module.exports = {
+  name: "8ball",
+  aliases: [],
+  description: "Ask the 8ball Anything!",
+  usage: "{prefix}8ball <question>",
+  run: async(client, message, args) => {
+		let question = args.slice(0).join(" ")
+let questions = [
+	"Not sure.",
+	"No",
+	"Never!",
+	"Of Course!",
+	"I am busy fixing your room mirror, ask me later!",
+	"Yes!",
+	"100% yes.",
+	"Ask me later"
+]
+
+let Result = questions[Math.floor(Math.random() * questions.length)];
+
+if(!question) return message.channel.send("Please make sure you are asking something to the 8ball!");
+
+message.reply(`The 8ball said ${Result}`)
+	}
+}
