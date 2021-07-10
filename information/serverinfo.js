232245213dddc8b05771b73b5b694ba80b613abc
DevFurry-1064 <82998889+DevFurry-1064@users.noreply.github.com>
2021-07-10 16:29:57 +0700

    Update serverinfo.js

--- information/serverinfo.js
+++ information/serverinfo.js
@@ -1,9 +1,12 @@
+const Discord = require('discord.js')
+const { Color } = require("../../config.js")
+
 module.exports = {
   name: "serverinfo",
   aliases: ["serveri", "si", "serverinformation"],
   description: "Gets the server's information",
   usage: "{prefix}serverinformation",
-  run: async(client, message, args, Discord, Color) => {
+  run: async(client, message, args) => {
   
   let Guild = message.guild;
   
@@ -13,11 +16,14 @@ module.exports = {
   let name = Guild.name
   let id = Guild.id
   let owner = Guild.owner.user.tag
+	let channels = Guild.channels.cache.size
   let roles = Guild.roles.cache.size
   let emojis = Guild.emojis.cache.size
   let totalmember = Guild.memberCount
   let humans = Guild.members.cache.filter(i => !i.user.bot).size
-  let bots = Guild.members.cache.filter(i => i.user.bot).size
+	let bots = Guild.members.cache.filter(i => i.user.bot).size
+	let boostLevel = Guild.premiumTier
+  let boosts = Guild.premiumSubscriptionCount
   
   //** Embed builder ** //
   
@@ -34,7 +40,11 @@ let embed = new Discord.MessageEmbed()
 .addField('Total Members', totalmember, true)
 .addField('Humans', humans, true)
 .addField('Bots', bots, true)
-.setColor(Color)
+.setThumbnail(iconURL)
+.setFooter(`Boost Level: ${boostLevel} | Boosts: ${boosts}`)
+.setColor(client.color)
+
+a.send(embed)
 }
 
 send(message.channel)
