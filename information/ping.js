16feb70c28b7a5cc1e0b9b89c8e7a216bf21d509
DevFurry-1064 <82998889+DevFurry-1064@users.noreply.github.com>
2021-07-10 16:29:56 +0700

    Update ping.js

--- information/ping.js
+++ information/ping.js
@@ -9,7 +9,7 @@ module.exports = {
    let m = await message.channel.send("Please wait, we are doing stuff to get the ping...");
 
 	 let embed = new Discord.MessageEmbed()
-	 .setDescription(`\`\`\`Bot ping: ${client.ws.ping}ms \nResponse time: ${new Date() - message.createdTimestamp}ms \nMessage Delay: ${new Date() - m.createdTimestamp}\`\`\``)
+	 .setDescription(`\`\`\`Bot ping: ${client.ws.ping}ms \nResponse time: ${new Date() - message.createdTimestamp}ms \nMessage Delay: ${new Date() - m.createdTimestamp}ms\`\`\``)
 	 .setColor(client.color)
 	 .setAuthor(`${client.user.username}'s latency information`)
 
