1ef1b4bc2bc532ccdb5a11743bb9aa4c3508806f
DevFurry-1064 <82998889+DevFurry-1064@users.noreply.github.com>
2021-07-10 16:29:55 +0700

    Update help.js

--- information/help.js
+++ information/help.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const db = require('quick.db')
 module.exports = {
   name: "help",
   aliases: [],
@@ -7,10 +8,29 @@ module.exports = {
   run: async(client, message, args) => {
 let commandName = args[0];
 
+let command = client.commands.get(commandName) || client.commands.get(client.aliases.get(commandName));
+
 let noName = new Discord.MessageEmbed()
 .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic: true}))
-.setDescription(`${client.user.username}'s commands! \n\n\`Information\`: \n${client.information.join(", ")} \n\n\`Fun:\` \n${client.fun.join(", ")} `)
+.setDescription(`${client.user.username}'s commands! \n\n\`Information\`: \n${client.information.join(", ")} \n\n\`Fun:\` \n${client.fun.join(", ")} \n\n\`Moderation:\` \n${client.moderation.join(", ")} \n\n\`Util:\` \n${client.util.join(", ")}`)
+.setColor(client.color)
+.setFooter('Total commands: ' + client.commands.size)
 
+if(!commandName) {
 message.channel.send(noName)
+} else if(commandName) {
+	if(command) {
+	let embed = new Discord.MessageEmbed()
+	.setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic: true}))
+	.addField('Command name', command.name, true)
+	.addField("Command description", command.description, true)
+	.addField("Command usage", command.usage.replace("{prefix}", `${db.get('prefix') || client.prefix}`), true)
+	.setColor(client.color)
+	
+	message.channel.send(embed)
+	} else {
+		message.channel.send(noName)
+	}
+}
 	}
 }
