759157a32586537e62127a64d7d90fe623b4dd4b
DevFurry-1064 <82998889+DevFurry-1064@users.noreply.github.com>
2021-07-11 12:09:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const colors = require("./colors.json")
+
 exports.prefix = "" // Your Bot's Prefix here
 exports.Color = "RANDOM" // Your Bot's Normal Embed Color
 exports.Token = "" // YOUR BOT TOKEN GOES HERE!
